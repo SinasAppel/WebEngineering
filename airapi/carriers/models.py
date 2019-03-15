69c0a9761f9d24b86b002e5db4abdaedd3146962
Jur Kroeze <j.kroeze.1@student.rug.nl>
2019-03-15 13:57:21 +0100

    Added carriers

--- airapi/carriers/models.py
+++ airapi/carriers/models.py
@@ -1,3 +1,13 @@
+import json
 from django.db import models
 
-# Create your models here.
+
+class Carrier(models.Model):
+    code = models.CharField(max_length=3)
+    name = models.CharField(max_length=200)
+
+
+    def toJSON(self):
+        data = {'name': self.name, 'code': self.code}
+        return json.dumps(data)
+
--- airapi/carriers/views.py
+++ airapi/carriers/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from .models import Carrier
 
 
 def index(request):
-    output = "Carrier"
-    return HttpResponse(output)
+    airport_code = request.GET.get('airport-code', False)
+    if airport_code:
+        return HttpResponse(airport_code)
+    else:
+        airport_list = Carrier.objects.order_by('code')
+        output = ', '.join([a.toJSON() for a in airport_list])
+        return HttpResponse(output)
