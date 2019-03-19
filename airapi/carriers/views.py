f263f3fe569cfd4f1c7483b319b781189ce70b12
Jur Kroeze <j.kroeze.1@student.rug.nl>
2019-03-19 22:35:25 +0100

    Minor improvement in speed

--- airapi/carriers/views.py
+++ airapi/carriers/views.py
@@ -7,24 +7,28 @@ from .models import CarrierData
 
 def index(request):
     airport_code = request.GET.get('airport-code', False)
-    carrier_list = []
+    unique_carriers = []
+    carrier_set = []
 
     # Depending on if the get request data contains an airport code (/v1/carriers?airport_code=)
     # create a carrier_list of carriers that should be returned
     if airport_code:
         carrier_data = CarrierData.objects.order_by('carrier__code')
+        print(carrier_data)
         for cd in carrier_data:
             if airport_code == cd.get_airport().get_code():
-                carrier_list.append(cd.get_carrier())
+                c = cd.get_carrier()
+                if c.to_json() not in unique_carriers:
+                    unique_carriers.append(c.to_json())
+                    carrier_set.append(c)
     else:
         carrier_list = Carrier.objects.order_by('code')
+        for a in carrier_list:
+            if a.to_json() not in unique_carriers:
+                unique_carriers.append(a.to_json())
+                carrier_set.append(a)
 
-    if carrier_list is []:
+    if carrier_set is []:
         return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
-    unique_carriers = []
-    carrier_set = []
-    for a in carrier_list:
-        if a.to_json() not in unique_carriers:
-            unique_carriers.append(a.to_json())
-            carrier_set.append(a)
+
     return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
