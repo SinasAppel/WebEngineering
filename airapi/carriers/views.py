16c0a4db10870289ab73cfb25a5c28fc51d4d92a
Jur Kroeze <j.kroeze.1@student.rug.nl>
2019-03-20 09:43:08 +0100

    Fixed the timing problem

--- airapi/carriers/views.py
+++ airapi/carriers/views.py
@@ -1,28 +1,29 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 import json
+
+from airports.models import Airport
 from .models import Carrier
-from .models import CarrierData
 
 
 def index(request):
     airport_code = request.GET.get('airport-code', False)
     unique_carriers = []
     carrier_set = []
+    carrier_list = Carrier.objects.order_by()
 
     # Depending on if the get request data contains an airport code (/v1/carriers?airport_code=)
     # create a carrier_list of carriers that should be returned
     if airport_code:
-        carrier_data = CarrierData.objects.order_by('carrier__code')
-        print(carrier_data)
-        for cd in carrier_data:
-            if airport_code == cd.get_airport().get_code():
-                c = cd.get_carrier()
-                if c.to_json() not in unique_carriers:
-                    unique_carriers.append(c.to_json())
-                    carrier_set.append(c)
+        airport_list = Airport.objects.order_by()
+        i = 0
+        for airport in airport_list:
+            if airport_code == airport.get_code():
+                if carrier_list[i].to_json() not in unique_carriers:
+                    unique_carriers.append(carrier_list[i].to_json())
+                    carrier_set.append(carrier_list[i])
+            i = i + 1
     else:
-        carrier_list = Carrier.objects.order_by('code')
         for a in carrier_list:
             if a.to_json() not in unique_carriers:
                 unique_carriers.append(a.to_json())
