a02b266644af5a22f204e2e2dac294323203a43a
DspatieJ <derk_spatie_jan@hotmail.com>
2019-03-21 22:06:11 +0100

    fixed urls

--- airapi/carriers/urls.py
+++ airapi/carriers/urls.py
@@ -4,19 +4,10 @@ from . import views, converters
 
 
 register_converter(converters.CarrierCodeConverter, 'ccode')
-register_converter(converters.AirportCodeConverter, 'acode')
-
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:return_type>', views.index, name='index'),
-    path('statistics/<ccode:code>/flights/<acode:airport>/<int:month>', views.statistics, name='statistics'),
-    path('statistics/<ccode:code>/flights/<acode:airport>/<int:month>/<str:return_type>', views.statistics, name='statistics'),
-    path('statistics/<ccode:code>/flights/delays/<acode:airport>/<int:month>', views.delays, name='delays'),
-    path('statistics/<ccode:code>/flights/delays/<acode:airport>/<int:month>/<str:return_type>', views.delays, name='delays'),
-    path('statistics/<ccode:code>/delay-minutes/<str:reason>/<acode:airport>/<int:month>', views.minutes, name='minutes'),
-    path('statistics/<ccode:code>/delay-minutes/<str:reason>/<acode:airport>/<int:month>/<str:return_type>', views.minutes, name='minutes'),
-    path('statistics/descriptive/<acode:airport_a>/<acode:airport_b>', views.descriptive, name='descriptive'),
-    path('statistics/descriptive/<acode:airport_a>/<acode:airport_b>/<str:return>', views.descriptive, name='descriptive'),
+    path('statistics/<ccode:carrier_code>/flights', views.flights, name='flights'),
+    path('statistics/<ccode:carrier_code>/delay-minutes', views.delays, name='delays'),
     path('time', views.time, name='time')
 ]
