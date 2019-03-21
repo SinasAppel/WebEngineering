768e8537069c4a05e7c6d198df953aaf3049ef35
DspatieJ <derk_spatie_jan@hotmail.com>
2019-03-21 22:28:34 +0100

    fixed missing url

--- airapi/carriers/urls.py
+++ airapi/carriers/urls.py
@@ -8,6 +8,7 @@ register_converter(converters.CarrierCodeConverter, 'ccode')
 urlpatterns = [
     path('', views.index, name='index'),
     path('statistics/<ccode:carrier_code>/flights', views.flights, name='flights'),
-    path('statistics/<ccode:carrier_code>/delay-minutes', views.delays, name='delays'),
+    path('statistics/<ccode:carrier_code>/flights/delays', views.delays, name='delays'),
+    path('statistics/<ccode:carrier_code>/delay-minutes', views.minutes, name='minutes'),
     path('time', views.time, name='time')
 ]
