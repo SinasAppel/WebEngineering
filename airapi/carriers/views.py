8588086e5234a93b2e90c73cdfb2319208f05c71
DspatieJ <derk_spatie_jan@hotmail.com>
2019-03-22 00:39:36 +0100

    #5 and #6 should now work correctly

--- airapi/carriers/views.py
+++ airapi/carriers/views.py
@@ -3,10 +3,10 @@ from django.http import JsonResponse
 import json
 
 from airports.models import Airport
-from .models import Carrier
+from .models import Carrier, Flights, Delays, MinutesDelayed, Time
 
 
-def index(request, return_type='json'):
+def index(request):
     airport_code = request.GET.get('airport-code', False)
     unique_carriers = []
     carrier_set = []
@@ -32,57 +32,150 @@ def index(request, return_type='json'):
     if carrier_set is []:
         return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
 
-    if return_type == 'json':
-        return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
+    return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
+
+
+def flights(request, carrier_code):
+    class Stats:
+        def __init__(self, f, a, c, t):
+            self.flights = f
+            self.airport = a
+            self.carrier = c
+            self.time = t
+
+        def dump(self):
+            return {"flights": {'cancelled': self.flights.get_cancelled(),
+                                'on time': self.flights.get_on_time(),
+                                'total': self.flights.get_total(),
+                                'delayed': self.flights.get_delayed(),
+                                'diverted': self.flights.get_diverted()},
+                    "airport": {'code': self.airport.get_code(),
+                                'name': self.airport.get_name()},
+                    "carrier": {'code': self.carrier.get_code(),
+                                'name': self.carrier.get_name()},
+                    "time": {'label': self.time.get_label(),
+                             'year': self.time.get_year(),
+                             'month': self.time.get_month()}
+                    }
+
+    airport_code = request.GET.get('airport-code', False)
+    month = int(request.GET.get('month', False))
+    year = int(request.GET.get('year', False))
+    if not airport_code:
+        return HttpResponse("Error, no airport-code given", status=400)
+
+    stat_list = []
+    flight_list = Flights.objects.order_by()
+    airports = Airport.objects.order_by()
+    carriers = Carrier.objects.order_by()
+    time = Time.objects.order_by()
+    length = len(carriers)
+    if not month or not year:
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code and airports[i].get_code() == airport_code:
+                s = Stats(flight_list[i], airports[i], carriers[i], time[i])
+                stat_list.append(s)
     else:
-        return HttpResponse(content='CSV response', status=200)
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code and airports[i].get_code() == airport_code:
+                if month == time[i].get_month() and year == time[i].get_year():
+                    s = Stats(flight_list[i], airports[i], carriers[i], time[i])
+                    stat_list.append(s)
+
+    return JsonResponse(json.dumps([s.dump() for s in stat_list]), safe=False)
 
 
-def statistics(request, code, airport, month, return_type='json'):
-    carrier_set = []
 
-    if carrier_set is []:
-        return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
 
-    if return_type == 'json':
-        return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
+def delays(request, carrier_code):
+    class Stats:
+        def __init__(self, d):
+            self.delays = d
+
+        def dump(self):
+            return {"delays": {'late aircraft': self.delays.get_late_aircraft(),
+                                'weather': self.delays.get_weather(),
+                                'security': self.delays.get_security(),
+                                'national aviation system': self.delays.get_national_aviation_system(),
+                                'carrier': self.delays.get_carrier()}
+                    }
+
+    airport_code = request.GET.get('airport-code', False)
+    month = int(request.GET.get('month', False))
+    year = int(request.GET.get('year', False))
+    if not airport_code:
+        return HttpResponse("Error, no airport-code given", status=400)
+
+    stat_list = []
+    flight_list = Delays.objects.order_by()
+    airports = Airport.objects.order_by()
+    carriers = Carrier.objects.order_by()
+    time = Time.objects.order_by()
+    length = len(carriers)
+    if not month or not year:
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code and airports[i].get_code() == airport_code:
+                s = Stats(flight_list[i])
+                stat_list.append(s)
     else:
-        return HttpResponse(content='CSV response', status=200)
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code and airports[i].get_code() == airport_code:
+                if month == time[i].get_month() and year == time[i].get_year():
+                    s = Stats(flight_list[i])
+                    stat_list.append(s)
+
+    return JsonResponse(json.dumps([s.dump() for s in stat_list]), safe=False)
 
 
-def delays(request, code, airport, month, return_type='json'):
-    carrier_set = []
+def minutes(request, carrier_code):
+    class Stats:
+        def __init__(self, d):
+            self.delays = d
 
-    if carrier_set is []:
-        return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
+        def dump(self):
+            return {"delays": {'late aircraft': self.delays.get_late_aircraft(),
+                               'weather': self.delays.get_weather(),
+                               'security': self.delays.get_security(),
+                               'national aviation system': self.delays.get_national_aviation_system(),
+                               'carrier': self.delays.get_carrier(),
+                               'total': self.delays.get_total()}
+                    }
 
-    if return_type == 'json':
-        return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
+
+    month = int(request.GET.get('month', False))
+    year = int(request.GET.get('year', False))
+    reason = request.GET.get('reason', False)
+
+    stat_list = []
+    flight_list = MinutesDelayed.objects.order_by()
+    carriers = Carrier.objects.order_by()
+    time = Time.objects.order_by()
+    length = len(carriers)
+    if not month or not year:
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code:
+                s = Stats(flight_list[i])
+                stat_list.append(s)
     else:
-        return HttpResponse(content='CSV response', status=200)
-
-
-def minutes(request, code, reason, airport, month, return_type='json'):
-    carrier_set = []
-
-    if carrier_set is []:
-        return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
-    if return_type == 'json':
-        return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
+        for i in range(length):
+            if carriers[i].get_code() == carrier_code:
+                if month == time[i].get_month() and year == time[i].get_year():
+                    s = Stats(flight_list[i])
+                    stat_list.append(s)
+    if reason == 'carrier':
+        return JsonResponse(json.dumps([s.delays.get_carrier() for s in stat_list]), safe=False)
+    elif reason == 'late_aircraft':
+        return JsonResponse(json.dumps([s.delays.get_late_aircraft() for s in stat_list]), safe=False)
+    elif reason == 'weather':
+        return JsonResponse(json.dumps([s.delays.get_weather() for s in stat_list]), safe=False)
+    elif reason == 'security':
+        return JsonResponse(json.dumps([s.delays.get_security() for s in stat_list]), safe=False)
+    elif reason == 'national_aviation_system':
+        return JsonResponse(json.dumps([s.delays.get_national_aviation_system() for s in stat_list]), safe=False)
+    elif reason == 'total':
+        return JsonResponse(json.dumps([s.delays.get_total() for s in stat_list]), safe=False)
     else:
-        return HttpResponse(content='CSV response', status=200)
-
-
-def descriptive(request, airport_a, airport_b, return_type='json'):
-    carrier_set = []
-
-    if carrier_set is []:
-        return HttpResponse(content="Error 503: List of carriers cannot be found.", status=503)
-
-    if return_type == 'json':
-        return JsonResponse(json.dumps([carrier.dump() for carrier in carrier_set]), safe=False, status=200)
-    else:
-        return HttpResponse(content='CSV response', status=200)
+        return JsonResponse(json.dumps([s.dump() for s in stat_list]), safe=False)
 
 
 def time(request):
