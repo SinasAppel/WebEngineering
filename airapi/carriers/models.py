6cc5642b2bdb1ea3d6a9d014f77c4b7b832facc6
DspatieJ <derk_spatie_jan@hotmail.com>
2019-03-21 23:58:33 +0100

    added fields for delays and minutesdelayed

--- airapi/carriers/models.py
+++ airapi/carriers/models.py
@@ -19,6 +19,9 @@ class Carrier(models.Model):
     def get_code(self):
         return self.code
 
+    def get_name(self):
+        return self.name
+
 
 class Flights(models.Model):
     cancelled = models.IntegerField()
@@ -26,6 +29,28 @@ class Flights(models.Model):
     total = models.IntegerField()
     delayed = models.IntegerField()
     diverted = models.IntegerField()
+
+    def dump(self):
+        return {"flights": {'cancelled': self.cancelled,
+                            'on time': self.on_time,
+                            'total': self.total,
+                            'delayed': self.delayed,
+                            'diverted': self.diverted}}
+
+    def get_cancelled(self):
+        return self.cancelled
+
+    def get_on_time(self):
+        return self.on_time
+
+    def get_total(self):
+        return self.total
+
+    def get_delayed(self):
+        return self.delayed
+
+    def get_diverted(self):
+        return self.diverted
 
 
 class Delays(models.Model):
@@ -35,6 +60,28 @@ class Delays(models.Model):
     national_aviation_system = models.IntegerField()
     carrier = models.IntegerField()
 
+    def dump(self):
+        return {"Delays": {'late aircraft': self.late_aircraft,
+                           'weather': self.weather,
+                           'security': self.security,
+                           'national aviation system': self.national_aviation_system,
+                           'carrier': self.carrier}}
+
+    def get_late_aircraft(self):
+        return self.late_aircraft
+
+    def get_weather(self):
+        return self.weather
+
+    def get_security(self):
+        return self.security
+
+    def get_national_aviation_system(self):
+        return self.national_aviation_system
+
+    def get_carrier(self):
+        return self.carrier
+
 
 class MinutesDelayed(models.Model):
     late_aircraft = models.IntegerField()
@@ -43,6 +90,32 @@ class MinutesDelayed(models.Model):
     security = models.IntegerField()
     total = models.IntegerField()
     national_aviation_system = models.IntegerField()
+
+    def dump(self):
+        return {"Minutes Delayed": {'late aircraft': self.late_aircraft,
+                                    'weather': self.weather,
+                                    'carrier': self.carrier,
+                                    'security': self.security,
+                                    'total': self.total,
+                                    'national aviation system': self.national_aviation_system}}
+
+    def get_late_aircraft(self):
+        return self.late_aircraft
+
+    def get_weather(self):
+        return self.weather
+
+    def get_security(self):
+        return self.security
+
+    def get_national_aviation_system(self):
+        return self.national_aviation_system
+
+    def get_carrier(self):
+        return self.carrier
+
+    def get_total(self):
+        return self.total
 
 
 class Statistics(models.Model):
@@ -56,6 +129,24 @@ class Time(models.Model):
     year = models.IntegerField()
     month = models.IntegerField()
 
+    def to_json(self):
+        data = {'label': self.label}
+        return json.dumps(data)
+
+    def dump(self):
+        return {"time": {'label': self.label,
+                         'year': self.year,
+                         'month': self.month}}
+
+    def get_label(self):
+        return self.label
+
+    def get_year(self):
+        return self.year
+
+    def get_month(self):
+        return self.month
+
 
 class CarrierData(models.Model):
     carrier = models.ForeignKey(Carrier, on_delete=models.CASCADE)
@@ -68,3 +159,4 @@ class CarrierData(models.Model):
 
     def get_airport(self):
         return self.airport
+
